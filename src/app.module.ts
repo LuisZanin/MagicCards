35e94f4c06407e2932849d4f888fd32bba1f2d9d
Luis Eduardo Zanin <leztoledo@gmail.com>
2024-08-26 13:24:16 -0300

    Iniciando Conexão

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

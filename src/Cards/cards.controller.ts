800c653728f2833ed8fb069b66b371c56cca47f0
Gfb <gfranzoi.barbosa@gmail.com>
2024-09-28 21:52:24 -0300

    feat: add caching to the route for listing decks for logged-in player

--- src/Cards/cards.controller.ts
+++ src/Cards/cards.controller.ts
@@ -1,22 +1,22 @@
-import { Controller, Get, Request, UseGuards } from "@nestjs/common";
+import { Controller, Get, Request, UseGuards, UseInterceptors, CacheInterceptor, CacheTTL } from "@nestjs/common";
 import { CardService } from "./cards.service";
-import { JwtAuthGuard } from "../auth/auth.guard"; 
-import { CreateCardDto } from './dto/create-cards.dto';
-
+import { JwtAuthGuard } from "../auth/auth.guard";
 
 @Controller('card')
 export class CardController {
     constructor(private readonly cardService: CardService) {}
 
-    @UseGuards(JwtAuthGuard)  // Protegendo a rota
+    @UseGuards(JwtAuthGuard)  // Protege a rota  
+    @UseInterceptors(CacheInterceptor)  // Ativa o cache para a rota
+    @CacheTTL(120)  // Define o tempo de cache para 120 segundos
     @Get('my-decks')
     async getMyDecks(@Request() req) {
         const userId = req.user.userId;  // Obtem o ID do jogador logado via JWT
-        return this.cardService.findDecksByPlayer(userId);
+        return this.cardService.findDecksByPlayer(userId);  
     }
 
     @Get('create')
-    async createCards(): Promise<CreateCardDto> {
+    async createCards(): Promise<any> {
         return this.cardService.create();
     }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MiddlewareConsumer } from '@nestjs/common';
 import { ResLogger } from './middleware';
-import { CardModule } from './cards/cards.module';
-//mongodb://localhost/magic
+import { CardModule } from './Cards/cards.module';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'), 
-    AuthModule, 
-    UsersModule, 
-    CardModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'),
+    AuthModule,
+    UsersModule,
+    CardModule,
+    CacheModule.register({
+      ttl: 60,  // Tempo de vida do cache
+      max: 100,  // Quantidade máxima de itens armazenados no cache
+    }),
+  ],
 })
 export class AppModule {
   //CASO RETIRAR MIDDLEWARE, RETIRAR CONSUMER E CONFIGURE.
-  configure(consumer: MiddlewareConsumer){
-    consumer.apply(ResLogger)
-    .forRoutes('*');
-}
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ResLogger).forRoutes('*');
+  }
 }
